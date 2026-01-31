7dfeb9ad5dec9f28e2d371e71b1d030d564686f1
Abdul Kareem <abdulkareemzahir9599@gmail.com>
2026-01-31 21:54:41 +0530

    frontend setup done!

--- server.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const connectDB = require('./config/db.js');
-const app = require('./src/app.js');
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-    connectDB();
-});
